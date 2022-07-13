89839b3ab1b83f70c7b08769316b4479af2d53fb
iql981 <iql981@my.utsa.edu>
2022-07-13 16:33:47 -0500

    ADD and ITEMS
    Added functionality for ADD and ITEMS commands

--- Program.cs
+++ Program.cs
@@ -10,21 +10,47 @@ namespace MultiValueDictionary
             Dictionary<string, string> dict = new Dictionary<string, string>();
             while (true)
             {
-                Console.Write(">");
+                Console.Write("> ");
                 string input = Console.ReadLine();
-                Console.WriteLine("You entered " + input);
-                if (input.StartsWith("ADD"))
+                string[] inputs = input.Split(' ');
+                if (inputs[0].Equals("ADD"))
                 {
-                    Add(dict, input);
+                    Add(dict, inputs);
+                }
+                else if (inputs[0].Equals("ITEMS"))
+                {
+                    Items(dict);
+                }
+                else
+                {
+                    Console.Write("Bad command\n");
                 }
             }
         }
-        private static void Add(Dictionary<string, string> dict, string input)
+        private static void Add(Dictionary<string, string> dict, string[] inputs)
         {
-            string[] inputs = input.Split(' ');
             string key = inputs[1];
             string value = inputs[2];
+            string valueFound;
+            if (dict.TryGetValue(key, out valueFound))
+            {
+                if (valueFound.Equals(value))
+                {
+                    Console.Write(") ERROR, member already exists for key\n");
+                    return;
+                }
+            }
             dict.Add(key, value);
+            Console.Write(") Added\n");
+        }
+        private static void Items(Dictionary<string, string> dict)
+        {
+            int i = 1;
+            foreach (KeyValuePair<string, string> kvp in dict)
+            {
+                Console.Write($"{i}) {kvp.Key}: {kvp.Value}\n");
+                i++;
+            }
         }
     }
 }
