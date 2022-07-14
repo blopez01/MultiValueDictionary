ee6d20a244877e8b764fd30dc3402e161bf58e3b
iql981 <iql981@my.utsa.edu>
2022-07-13 20:31:47 -0500

    More Optimizations and Documentation

--- Program.cs
+++ Program.cs
@@ -12,44 +12,45 @@ namespace MultiValueDictionary
             {
                 Console.Write("> ");
                 string input = Console.ReadLine();
-                string[] inputs = input.Split(' ');
-                if (inputs[0].Equals("KEYS"))
+                string[] inputs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = inputs[0];
+                if (command.Equals("KEYS"))
                 {
                     Keys(dict);
                 }
-                else if (inputs[0].Equals("MEMBERS"))
+                else if (command.Equals("MEMBERS"))
                 {
                     Members(dict, inputs);
                 }
-                else if (inputs[0].Equals("ADD"))
+                else if (command.Equals("ADD"))
                 {
                     Add(dict, inputs);
                 }
-                else if (inputs[0].Equals("REMOVE"))
+                else if (command.Equals("REMOVE"))
                 {
                     Remove(dict, inputs);
                 }
-                else if (inputs[0].Equals("REMOVEALL"))
+                else if (command.Equals("REMOVEALL"))
                 {
                     RemoveAll(dict, inputs);
                 }
-                else if (inputs[0].Equals("CLEAR"))
+                else if (command.Equals("CLEAR"))
                 {
                     Clear(dict);
                 }
-                else if (inputs[0].Equals("KEYEXISTS"))
+                else if (command.Equals("KEYEXISTS"))
                 {
                     KeyExists(dict, inputs, true);
                 }
-                else if (inputs[0].Equals("MEMBEREXISTS"))
+                else if (command.Equals("MEMBEREXISTS"))
                 {
                     MemberExists(dict, inputs, true);
                 }
-                else if (inputs[0].Equals("ALLMEMBERS"))
+                else if (command.Equals("ALLMEMBERS"))
                 {
                     AllMembers(dict);
                 }
-                else if (inputs[0].Equals("ITEMS"))
+                else if (command.Equals("ITEMS"))
                 {
                     Items(dict);
                 }
